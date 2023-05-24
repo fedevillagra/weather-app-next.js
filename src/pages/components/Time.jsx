ff28e75d37c65b395e1d5187c2252f5577493084
Federico Villagra <federico.villagra@mi.unc.edu.ar>
2023-05-24 06:29:21 -0300

    trying to fix deployment

--- src/pages/components/Time.jsx
+++ src/pages/components/Time.jsx
@@ -3,14 +3,14 @@ import { ThemeContext } from "@/ThemeContext";
 
 const Time = ({ onHoraActualChange }) => {
   const { weatherData } = useContext(ThemeContext);
-  const UTC = weatherData.timezone / 3600;
+  const UTC = weatherData?.timezone / 3600;
 
   const obtenerHoraActual = () => {
     const fechaActual = new Date();
     fechaActual.setHours(fechaActual.getUTCHours() + UTC);
-    return fechaActual.toLocaleTimeString('es-ES', {
-      hour: 'numeric',
-      minute: '2-digit',
+    return fechaActual.toLocaleTimeString("es-ES", {
+      hour: "numeric",
+      minute: "2-digit",
     });
   };
 
@@ -23,14 +23,16 @@ const Time = ({ onHoraActualChange }) => {
 
   const actualizarHora = () => {
     const nuevaHoraActual = obtenerHoraActual();
-    (nuevaHoraActual !== 'Invalid Date') && setHoraActual(nuevaHoraActual);
-    onHoraActualChange(nuevaHoraActual); // Llamar a la función de callback con la nueva hora actual
+    if (nuevaHoraActual !== "Invalid Date") {
+      setHoraActual(nuevaHoraActual);
+      onHoraActualChange(nuevaHoraActual);
+    }
     requestAnimationFrame(actualizarHora);
   };
 
   return (
     <div className="fixed bottom-0 right-0 z-50">
-      {horaActual !== 'Invalid Date' && (
+      {horaActual !== "Invalid Date" && (
         <h1 className="text-5xl font-light tracking-tight text-white lg:text-6xl px-10 py-8">
           {horaActual}
         </h1>
